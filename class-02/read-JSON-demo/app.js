51c277e77d618356e4947fe3b3cd4ffccfad4af2
LENA EIVY <applena@gmail.com>
2019-12-03 17:04:15 -0800

    day 2 updated

--- class-02/read-JSON-demo/app.js
+++ class-02/read-JSON-demo/app.js
@@ -1,53 +1,41 @@
-'use strict';
-
 // GOAL: render each dog and their info to the index page
-// {
-//   "name": "Ginger, Destroyer of Pens",
-//   "image_url": "ginger.jpeg",
-//   "hobbies": "Eating Cables"
-// },
+  // call a render method on each dog instance
 
-const allDogs = [];
+const dogArray = [];
 
-function Dog(name, image_url, hobbies){
-  this.name = name;
-  this.image_url = image_url;
-  this.hobbies = hobbies;
+function Dog(dogObj){
+  this.name = dogObj.name;
+  this.image_url = dogObj.image_url;
+  this.hobbies = dogObj.hobbies;
 
-  allDogs.push(this);
+  dogArray.push(this);
 }
 
 Dog.prototype.render = function(){
-  // select all the html in the dog template
+  // make a template
   const myTemplate = $('#dog-template').html();
-  
-  // make a new section to hold my template
+
+  // make a new section
   const $newSection = $('<section></section>');
 
-  // put the html from the dog template into the new section
+  // put the template html into my new section
   $newSection.html(myTemplate);
 
-  // find the h2 and fill the text with the name
+  // find the h2 and fill it with the name
   $newSection.find('h2').text(this.name);
+
+  // find the img and fill the src and alt
+  $newSection.find('img').attr('src', this.image_url);
+  $newSection.find('img').attr('alt', this.name);
 
   // find the p tag and fill with hobbies
   $newSection.find('p').text(this.hobbies);
 
-  // find the image_url and fill with the image url
-  $newSection.find('img').attr('src', this.image_url);
-
-  // stick my template onto the DOM
   $('main').append($newSection);
 }
 
-//AJAX 
-$.get('./data.json', data => {
+$.get('data.json', data => {
   data.forEach(dog => {
-    new Dog(dog.name, dog.image_url, dog.hobbies).render();
+    new Dog(dog).render();
   })
 })
-
-  // use ajax to get the data file
-  // then run each dog through the constructor function
-  // then call a render method on each dog instance
-    // we will need an array to hold all the dog instances
